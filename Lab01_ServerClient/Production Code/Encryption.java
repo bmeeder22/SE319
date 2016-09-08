1d59068cd5adb5d9fcccd12bc1306af156a0bae3
Noah Eigenfeld <noah.eigenfeld@jamfsoftware.com>
2016-09-08 13:09:07 -0500

    File encryption seems to be working - still need to add decryption

--- Lab01_ServerClient/Production Code/Encryption.java
+++ Lab01_ServerClient/Production Code/Encryption.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Encryption {
 	final private static byte key = (byte) 240; // key: 11110000
@@ -49,41 +50,81 @@ public class Encryption {
 		return alternateBytes;
 	}
 
-	public static byte[] encryptFile(File file) {
+	public static byte[] encryptFileToByteArray(File file) throws IOException {
 		byte[] encryptedFileBytes = new byte[0];
 		try {
 			Path path = Paths.get(file.getAbsolutePath());
 			byte[] fileBytes = Files.readAllBytes(path);
+			ArrayList<Integer> fileBitsIntArray = getBitsIntArray(fileBytes);
 
-			if (fileBytes.length % 3 != 0) {
-				System.out.println("file bytes not divisible by 3");
-			}
+			// System.out.println("Bytes in file: " + fileBytes.length);
+			// int bytesAdded = 0;
 
-			int everyThree = 0;
-			for (int i = 0; i < fileBytes.length; i++) {
+			int bitNum = 7;	//counts down from 7 to 2
+			byte encryptedByte = Byte.parseByte("0"); //creates a byte of all 0s
+			for (int bit : fileBitsIntArray) {
+				encryptedByte = setBit(encryptedByte, bitNum, bit);
 				
+				if (bitNum == 2) { //add it to encryptedFileBytes
+					encryptedFileBytes = appendByte(encryptedFileBytes, encryptedByte);
 
-				everyThree = (everyThree + 1) % 3;
+					//TODO: Remove the following block - for testing only
+					// bytesAdded++;
+					// byte[] singleByte = new byte[0];
+					// singleByte = appendByte(singleByte, encryptedByte);
+					// System.out.println("Encrypted byte " + bytesAdded + ": " + getBitsIntArray(singleByte).toString());
+					
+					bitNum = 7;
+				} else {
+					bitNum--;
+				}
 			}
+
+			// System.out.println("Bytes written: " + encryptedFileBytes.length);
 
 			return encryptedFileBytes;
 		} catch (IOException e) {
-			System.out.println("Something went wrong.");
+			System.out.println("Something went wrong. Could not encrypt file.");
 			return null;
 		}
 	}
 
-	private static byte getBit(byte b, int bitNum) {
-		return (b >> bitNum) & 1;
+	public static File decryptFileFromByteArray(byte[] encryptedFileBytes) {
+		File file = null;
+
+		//TODO
+
+		return file;
 	}
 
-//	private static byte setBit(byte b, byte bitNum, byte bitValue) {
-//		if (bitValue == 1) {
-//			b = b | (1 << bitNum);
-//		} else if (bitValue == 0) {
-//			b = b & (0 << bitNum);
-//		}
-//	}
+	//-----------------------------------------
+	// HELPER METHODS
+	//-----------------------------------------
+
+	private static int getBit(byte b, int bitNum) {
+		return ((b >> bitNum) & 1);
+	}
+
+	private static byte setBit(byte b, int bitNum, int bitValue) {
+		byte newByte = (byte) 0;
+		if (bitValue == 1) {
+			newByte = (byte) (b | (1 << bitNum));
+		} else if (bitValue == 0) {
+			newByte = (byte) (b & ~(1 << bitNum));
+		}
+		return newByte;
+	}
+	
+	private static ArrayList<Integer> getBitsIntArray(byte[] bytes) {
+		ArrayList<Integer> bits = new ArrayList<Integer>();
+		for (byte b : bytes) {
+			for (int bitNum = 7; bitNum >= 0; bitNum--) {
+				bits.add(getBit(b, bitNum));				
+			}
+		}
+		
+		return bits;
+	}
 
 	private static byte[] appendBytes(byte[] bytes1, byte[] bytes2) {
 		byte[] result = new byte[bytes1.length + bytes2.length];
@@ -101,19 +142,51 @@ public class Encryption {
 		return result;
 	}
 	
+	private static byte[] appendByte(byte[] bytes, byte toAppend) {
+		byte[] result = new byte[bytes.length + 1];
+		
+		int index = 0;
+		for (byte b : bytes) {
+			result[index] = b;
+			index++;
+		}
+		result[index] = toAppend;
+		
+		return result;
+	}
+	
 	//TODO: Remove main method - for testing purposes only
-	public static void main(String[] args) {
-		System.out.println("Original string: Hello World");
-		byte[] encryptedString = encryptString("Hello World");
-		System.out.println("Decrypted string: " + decryptString(encryptedString));
+	// public static void main(String[] args) throws IOException {
+		// System.out.println("Original string: Hello World");
+		// byte[] encryptedString = encryptString("Hello World");
+		// System.out.println("Decrypted string: " + decryptString(encryptedString));
 
-		File test = new File("test.txt");
-		encryptFile(test);
+		// byte one = (byte) 1;
+		// byte two = (byte) 2;
+		// System.out.println("One: " + getBit(one, 3) + getBit(one, 2) + getBit(one, 1) + getBit(one, 0));
+		// System.out.println("Two: " + getBit(two, 3) + getBit(two, 2) + getBit(two, 1) + getBit(two, 0));
 
-		byte one = (byte) 1;
-		byte two = (byte) 2;
-		System.out.println("One: " + getBit(one, 0));
-		System.out.println("Two: " + getBit(two, 1));
+		// two = setBit(two, 1, 0);
+		// System.out.println("Zero: " + getBit(two, 3) + getBit(two, 2) + getBit(two, 1) + getBit(two, 0));
+		// two = setBit(two, 2, 1);
+		// System.out.println("Four: " + getBit(two, 3) + getBit(two, 2) + getBit(two, 1) + getBit(two, 0));
+		// two = setBit(two, 3, 1);
+		// System.out.println("Twelve: " + getBit(two, 3) + getBit(two, 2) + getBit(two, 1) + getBit(two, 0));
+		// two = setBit(two, 2, 0);
+		// System.out.println("Eight: " + getBit(two, 3) + getBit(two, 2) + getBit(two, 1) + getBit(two, 0));
+		
+		// byte[] numbersByte = new byte[0];
+		// numbersByte = appendByte(numbersByte, Byte.parseByte("1"));
+		// numbersByte = appendByte(numbersByte, Byte.parseByte("3"));
+		// numbersByte = appendByte(numbersByte, Byte.parseByte("15"));
+		// numbersByte = appendByte(numbersByte, Byte.parseByte("-128"));
+		// ArrayList<Integer> numbersBitArray = getBitsIntArray(numbersByte);
+		// System.out.println(numbersBitArray.toString());
+
+		// File test = new File("test.txt");
+		// byte[] encryptedFileBytes = encryptFileToByteArray(test);
+
+		// System.out.println("Encrypted File: " + getBitsIntArray(encryptedFileBytes).toString());
 	}
 
 }
