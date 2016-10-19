5ec377c13b5f05993565576c9f28ba38bdc41c41
Noah Eigenfeld <noah.eigenfeld@jamfsoftware.com>
2016-10-19 16:05:15 -0500

    Cleaning up

--- Exercise05_js_ajax/makeMessage.php
+++ Exercise05_js_ajax/makeMessage.php
@@ -17,23 +17,6 @@ function rsa_encrypt($string, $public_key)
     return $cipher->encrypt($string);
 }
 
-//Function for decrypting with RSA 
-function rsa_decrypt($string, $private_key)
-{
-    //Create an instance of the RSA cypher and load the key into it
-    $cipher = new Crypt_RSA();
-    $cipher->loadKey($private_key);
-    //Set the encryption mode
-    $cipher->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
-    //Return the decrypted version
-    return $cipher->decrypt($string);
-}
-
-//Test out the rsa encryption functions
-// $plaintext = "This is some plaintext to encrypt";
-// $ciphertext = rsa_encrypt($plaintext, $public_key);
-// $decipheredtext = rsa_decrypt($ciphertext, $private_key);
-
 //-----------------------END ExampleCryptography.php----------------------------
 
 function getUserPublicKey($username) {
@@ -49,10 +32,9 @@ function getUserPublicKey($username) {
 function getUsersArray($file) {
     $users = [];
     $fullUserStrings = explode("&", $file);
-    // var_dump($fullUserStrings);
+
     foreach ($fullUserStrings as &$userString) {
-        // var_dump($userString);
-        // echo"<br>";
+        //check if string is empty
         if (strcmp($userString, '') != 0) {
             $userInfo = explode(":", $userString);
 
@@ -60,71 +42,18 @@ function getUsersArray($file) {
                 username => $userInfo[0],
                 publickey => $userInfo[2]
             ];
-            array_push($users, $user);
-            // echo "Next User: ";
-            // var_dump($user);
-            // echo "<br>";
+            array_push($users, $user); //add username and public key to array
         }
-
     }
     return $users;
 }
 
-function addMessageToFile($messages, $public_key) {
-    // $myfile = fopen("messages.txt", "a");
-    global $file;
-
-    $encryptedText = rsa_encrypt($_GET['body'], $public_key);
-
-    $txt = $_GET['receiver'].'STRING-BREAK'.$_SESSION['user'].'STRING-BREAK'.$encryptedText."END-MESSAGE";
-
-    $newFile = $file.$txt;
-
-    file_put_contents("messages.txt", $newFile);
-    // fclose($myfile);
-
-    // $message = [
-    //     'receiver' => $_GET['receiver'],
-    //     'sender' => $_SESSION['user'],
-    //     'body' => $encryptedText
-    // ];
-
-    // array_push($messages, $message); //add encrypted message to the array of message objects
-    // echo "Messages: ";
-    // echo "<br>";
-    // echo "<br>";
-    // echo "<br>";
-    // var_dump($messages);
-    // $JSON = json_encode($messages); //convert array of message objects back to JSON
-
-    // echo $JSON;
-
-    // file_put_contents('messages.txt', $JSON); //save JSON to file
-}
-
-function convertJSONtoArray($string) {
-    $messages = json_decode($string);
-    $messageArray = [];
-
-    for($i = 0; $i<count($messages); $i++) {
-        $messageObject = $messages[$i];
-        $message = get_object_vars($messageObject);
-        array_push($messageArray, $message);
-    }
-
-    return $messageArray;
-}
-
 //----------------------END FUNCTION DECLARATIONS-----------------------
-session_start();
 
-//Get public and private keys
-$private_key = $_SESSION['privatekey'];
-$public_key = $_SESSION['publickey'];
+session_start();
 
 $usersFile = file_get_contents('users.txt');
 $users = getUsersArray($usersFile);
-// var_dump($users);
 
 $public_key = getUserPublicKey($_GET['receiver']);
 
@@ -133,11 +62,15 @@ if (strcmp($public_key, "USER NOT FOUND") == 0) {
     exit();
 }
 
-$file = file_get_contents('messages.txt'); //retrieve contents of file in JSON format
+$file = file_get_contents('messages.txt'); //retrieve contents of file
 
-$encryptedMessages = convertJSONtoArray($file); //transform JSON into an array of message objects
+$encryptedText = rsa_encrypt($_GET['body'], $public_key); //encrypt the body of the message
+//construct string to be saved
+$txt = $_GET['receiver'].'STRING-BREAK'.$_SESSION['user'].'STRING-BREAK'.$encryptedText."END-MESSAGE";
 
-addMessageToFile($encryptedMessages, $public_key);
+$newFile = $file.$txt; //add new message to existing messages
+
+file_put_contents("messages.txt", $newFile); //save all messages back to the file
 
 
 
