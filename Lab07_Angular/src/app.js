9ab6f7a806bc0b22fff7932d92c1ec109e76528c
Noah Eigenfeld <noah.eigenfeld@jamfsoftware.com>
2016-11-12 20:06:08 -0600

    logging in as admin works

--- Lab07_Angular/src/app.js
+++ Lab07_Angular/src/app.js
@@ -1,15 +1,31 @@
 angular.module('myApp', ['ngRoute']) //ngRoute is an angular service
 .config(function ($routeProvider) {
-    $routeProvider.when("/login", {
+    $routeProvider.when("/", {
         controller: "loginController",
         templateUrl: "login.html"
+    });
+    $routeProvider.when("/librarian", {
+        controller: "librarianView",
+        templateUrl: "librarian.html"
     });
 })
 .controller('indexController', function($scope) {
 })
-.controller('loginController', function($scope) {
+.controller('loginController', function($scope, $location) {
     $scope.name = "Login";
     $scope.username = "";
     $scope.password = "";
+
+    $scope.checkLogin = function() {
+        console.log("Logging in...");
+        if ($scope.username == "admin" && $scope.password == "admin") {
+            console.log("User is admin");
+            $location.path("/librarian");
+        }
+    }
+})
+.controller('librarianController', function($scope) {
+    $scope.name = "Librarian";
+    $scope.username = "admin";
 });
 
